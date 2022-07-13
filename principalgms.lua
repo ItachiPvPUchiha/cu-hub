30dc8db36d0fd1b7dbd9586136e0b77e0634d8f8
ItachiPvPUchiha <88865128+ItachiPvPUchiha@users.noreply.github.com>
2022-07-12 22:53:00 -0300

    Update principalgms.lua

--- principalgms.lua
+++ principalgms.lua
@@ -1,7 +1,7 @@
 repeat until game:IsLoaded()
 if game.PlaceId == 8304191830 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/ItachiPvPUchiha/cu-hub/main/adv.lua"))()
-elseif game.PlaceId == 2809202155 then
+elseif game.PlaceId == 2809202155 or 4643697430 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/ItachiPvPUchiha/cu-hub/main/yba.lua"))()
 else
     game.Players.LocalPlayer:kick("Not supported.")
